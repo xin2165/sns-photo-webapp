4579ad3c374ba163377fd8ee84487bc154caa533
xinzhou <xin.2165@gmail.com>
2017-03-09 15:34:35 +0800

    modify ws

--- ws/src/main/scala/org/openurp/sns/photo/web/DefaultModule.scala
+++ ws/src/main/scala/org/openurp/sns/photo/web/DefaultModule.scala
@@ -3,11 +3,14 @@ package org.openurp.sns.photo.web
 import org.beangle.commons.inject.bind.AbstractBindModule
 import org.beangle.data.jdbc.query.JdbcExecutor
 import org.openurp.sns.photo.web.action.SearchAction
+import org.beangle.cache.ehcache.EhCacheManager
 
 class DefaultModule extends AbstractBindModule {
 
   protected override def binding() {
     bind(classOf[SearchAction])
     bind(classOf[JdbcExecutor])
+    
+    bind("cache.Ehcache", classOf[EhCacheManager]).constructor("ehcache-photo",false)
   }
 }
--- ws/src/main/scala/org/openurp/sns/photo/web/action/SearchAction.scala
+++ ws/src/main/scala/org/openurp/sns/photo/web/action/SearchAction.scala
@@ -13,23 +13,39 @@ import java.io.FileInputStream
 import java.text.SimpleDateFormat
 import javax.activation.MimeType
 import java.io.ByteArrayInputStream
+import org.beangle.cache.ehcache.EhCacheManager
+import org.beangle.webmvc.api.view.StreamView
 
-class SearchAction extends ActionSupport {
+class SearchAction(ehCacheManager: EhCacheManager) extends ActionSupport {
 
   var jdbcExecutor: JdbcExecutor = _
 
+  val cache = ehCacheManager.getCache("photo", classOf[String], classOf[Object])
+
   @mapping("{photoId}")
   def index(@param("photoId") photoId: String): View = {
-    Stream(new ByteArrayInputStream(loadFromDB(photoId)), "image/jpg", photoId+".jpg")
+    cache.get(photoId) match {
+      case Some(image) => buildStream(image.asInstanceOf[Array[Byte]], photoId)
+      case None =>
+        loadFromDB(photoId) match {
+          case Some(image) =>
+            cache.put(photoId, image)
+            buildStream(image, photoId)
+          case None =>
+            val nfile = new File(this.getClass().getClassLoader().getResource("DefaultPhoto.gif").getFile())
+            val in = new FileInputStream(nfile)
+            buildStream(new Array[Byte](nfile.length.toInt), photoId)
+        }
+    }
   }
 
-  def loadFromDB(photoId: String, size: String = "small"): Array[Byte] = {
+  private def buildStream(bytes: Array[Byte], photoId: String): StreamView = {
+    Stream(new ByteArrayInputStream(bytes), "image/jpg", photoId + ".jpg")
+  }
+
+  def loadFromDB(photoId: String, size: String = "small"): Option[Array[Byte]] = {
     val rs = jdbcExecutor.query("select user_id, updated_at from photo.photo_infos where photo_id =?", photoId)
-    if (rs.isEmpty) {
-      val nfile = new File(this.getClass().getClassLoader().getResource("DefaultPhoto.gif").getFile())
-      val in = new FileInputStream(nfile)
-      new Array[Byte](nfile.length.toInt)
-    } else {
+    if (!rs.isEmpty) {
       val row = rs.head
       val year = row(0).toString.substring(0, 4)
       val image = if (size == "small") {
@@ -37,7 +53,9 @@ class SearchAction extends ActionSupport {
       } else {
         jdbcExecutor.query("select origin_image from photo.photos" + year + " where id =?", photoId).head.head.asInstanceOf[Array[Byte]]
       }
-      image
+      Some(image)
+    } else {
+      None
     }
   }
 }
